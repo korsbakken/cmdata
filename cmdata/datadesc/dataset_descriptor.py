45d7623b8ac2d10a9d9bbecc7d704f82df91c83b
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2022-09-25 11:52:58 +0200

    Refactored dataset_descriptor.py
    Separated non-dataset-specific attributes and functionality into DescriptorBase
    class, and dataset-specific attributes into DatasetDescriptorBase.
    
    Has not been tested.

--- cmdata/datadesc/dataset_descriptor.py
+++ cmdata/datadesc/dataset_descriptor.py
@@ -11,39 +11,26 @@ from . import helpers
 from . import yamlfuncs
 
 
-class DatasetDescriptorBase(pydantic.BaseModel):
-    """Basis for dataset descriptor objects."""
+class DescriptorBase(pydantic.BaseModel):
+    """Basis for all descriptor objects."""
     id: str
-    """The dataset id (short name). Should be a valid Python variable name."""
+    """Id (short name). Should be a valid Python variable name."""
     name: tp.Optional[str]
-    """Dataset full name. Optional (but highly recommended)"""
+    """Full name. Optional (but highly recommended)"""
     parent_id: tp.Optional[str]
     """ID of parent dataset or dataset collection. Optional (and may not be
     applicable at all)."""
     description: tp.Optional[str]
-    """Free-form description of the dataset. Optional"""
-    raw_data_path: tp.Optional[tp.Union[Path, tp.Dict[str, Path]]]
-    """Path to raw data file(s). Either a single Path instance, or a dict of
-    Path instances. If a dict, the keys will be interpreted as version
-    identifiers. Optional."""
-    default_version: tp.Optional[str]
-    """Default version of the dataset, if there are multiple versions. If
-    specified, it is assumed that `raw_data_path` (if it itself is specified)
-    is a dict with version ids as keys."""
-    dimensions: tp.Optional[tp.Tuple[str, ...]] = ()
-    """Tuple with (ordered) names of dimensions in the dataset, if supported.
-    Optional, by default set to an empty tuple (but can be explicitly set to 
-    None if desired, to explicitly show if the concept of dimensions is not
-    relevant to the given dataset)."""
+    """Free-form description. Optional"""
     notes: tp.Optional[str]
-    """Notes about the dataset, as a single string. Optional."""
+    """Notes, as a single string. Optional."""
 
     def with_base_path(
         self,
         basepath: tp.Union[Path, str],
         make_absolute: bool = True,
         inplace: bool = False
-    ) -> DatasetDescriptorBase:
+    ) -> DescriptorBase:
         """Prefixes a root/base path to all Path objects in `self`.
         
         The path is set recursively, i.e., it also includes Path objects in
@@ -63,7 +50,7 @@ class DatasetDescriptorBase(pydantic.BaseModel):
         
         Returns
         -------
-        DatasetDescriptorBase
+        DescriptorBase
             The object itself (if `inplace` is True) or a copy with the prefixed
             base path."""
         basepath = Path(basepath)
@@ -72,33 +59,33 @@ class DatasetDescriptorBase(pydantic.BaseModel):
             action = lambda p: (basepath / p).absolute()
         else:
             action = lambda p: basepath / p
-        new_obj: DatasetDescriptorBase = helpers.recurse_by_type(
+        new_obj: DescriptorBase = helpers.recurse_by_type(
             self,
             match_type=Path,
             action=action,
             inplace=inplace
         )
         return new_obj
-    ###END def DatasetDescriptorBase.with_base_path
+    ###END def DescriptorBase.with_base_path
 
     @classmethod
     def dict_from_yaml(
         cls,
-        datasets_file: tp.Union[Path, str],
-        datasets_key: str = 'datasets',
+        descriptors_file: tp.Union[Path, str],
+        descriptors_key: str = 'datasets',
         parse_func: tp.Callable[[yamlfuncs._YamlDataType],
                                  yamlfuncs._YamlDataType] \
             = yamlfuncs.resolve_variables
-    ) -> tp.Dict[str, DatasetDescriptorBase]:
-        """Load a dataset descriptors from a YAML file.
+    ) -> tp.Dict[str, DescriptorBase]:
+        """Load descriptors from a YAML file.
         
         Parameters
         ----------
-        datasets_file : Path or str
+        descriptors_file : Path or str
             Name or Path instance of the yaml file to load from.
-        datasets_key : str, optional
-            The key that the dataset descriptions are found under in the yaml
-            file. Optional, by default `'datasets'`.
+        descriptors_key : str, optional
+            The key that the  descriptions are found under in the yaml file.
+            Optional, by default `'datasets'`.
         parse_func : callable
             Function to parse the yaml file contents. Must return a data
             structure similar to what would be returned by
@@ -107,28 +94,44 @@ class DatasetDescriptorBase(pydantic.BaseModel):
 
         Returns
         -------
-        Dict of DatasetDescriptorBase instances
-            A dict of dataset descriptor instances. The keys are the dataset
-            IDs, prefixed with parent IDs (if present) followed by underscore.
-            If parent IDs are not present, the dataset IDs alone are used as
-            keys.
+        Dict of DescriptorBase instances
+            A dict of descriptor instances. The keys are the IDs, prefixed with
+            parent IDs (if present) followed by underscore. If parent IDs are
+            not present, the IDs alone are used as keys.
         """
         _YamlDataType = yamlfuncs._YamlDataType
-        with open(datasets_file, mode='r') as f:
+        with open(descriptors_file, mode='r') as f:
             data: _YamlDataType = yamlfuncs.load_yaml(f)
         data = parse_func(data)
-        datasets: tp.Dict[str, DatasetDescriptorBase] = dict()
+        datasets: tp.Dict[str, DescriptorBase] = dict()
         _dataset: tp.Dict[str, tp.Any]
-        _descr: DatasetDescriptorBase
-        for _dataset in data[datasets_key]:
-            _descr = DatasetDescriptorBase(**_dataset)
+        _descr: DescriptorBase
+        for _dataset in data[descriptors_key]:
+            _descr = DescriptorBase(**_dataset)
             if _descr.parent_id is not None:
                 _dataset_key: str = f'{_descr.parent_id}_{_descr.id}'
             else:
                 _dataset_key: str = f'{_descr.id}'
             datasets[_dataset_key] = _descr
         return datasets
-    ###END def DatasetDescriptorBase.from_yaml
+    ###END def DescriptorBase.from_yaml
+
+###END class DescriptorBase
 
 
+class DatasetDescriptorBase(DescriptorBase):
+    """Basis for dataset descriptor objects."""
+    raw_data_path: tp.Optional[tp.Union[Path, tp.Dict[str, Path]]]
+    """Path to raw data file(s). Either a single Path instance, or a dict of
+    Path instances. If a dict, the keys will be interpreted as version
+    identifiers. Optional."""
+    default_version: tp.Optional[str]
+    """Default version of the dataset, if there are multiple versions. If
+    specified, it is assumed that `raw_data_path` (if it itself is specified)
+    is a dict with version ids as keys."""
+    dimensions: tp.Optional[tp.Tuple[str, ...]] = ()
+    """Tuple with (ordered) names of dimensions in the dataset, if supported.
+    Optional, by default set to an empty tuple (but can be explicitly set to 
+    None if desired, to explicitly show if the concept of dimensions is not
+    relevant to the given dataset)."""
 ###END class DatasetDescriptorBase
