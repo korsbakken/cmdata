ec5990a8598437720b42683ab58bdc37d5f4a5fe
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2022-10-07 22:31:42 +0200

    Added pandas plotting functionality to helpers.xr_utils.

--- helpers/xr_utils.py
+++ helpers/xr_utils.py
@@ -17,6 +17,7 @@ register_tsutils(name: str = 'tsutils', xrtype: str = 'both')
 import typing as tp
 
 import xarray as xr
+import pandas as pd
 
 XrObj: type = tp.Union[xr.Dataset, xr.DataArray]
 
@@ -68,6 +69,90 @@ class XrTsUtils:
 
 ###END class XrTsUtils
 
+
+class XrPlotUtils:
+    """Xarray accessor class for extra plotting-related functionality."""
+
+    __slots__ = ('_xrobj')
+    _xrobj: XrObj
+
+    def __init__(self, xrobj: XrObj):
+        self._xrobj: tp.Union[xr.Dataset, xr.DataArray] = xrobj
+    ###END def XrPlotUtils.__init__
+
+    def pdplot(
+        self,
+        y: tp.Union[str, tp.List[str]] = None,
+        kind: str = None,
+        **kwargs
+    ) -> tp.Any:
+        """Make plots leveraging pandas `Dataframe.plot` functions.
+        
+        This function transforms a Dataset or DataArray into a pandas Series or
+        DataFrame, and calls functions under its `plot` attribute, which ofers
+        more plot types than `xarray` does natively through `Dataset.plot` or
+        `DataArray.plot`.
+
+        Parameters
+        ----------
+        x : str or any, optional
+            Dimension to use for the `x` argument of pandas plotting functions.
+            This usually does not need to be specified. By default, all
+            dimensions will be used (and will be levels of the MultiIndex of the
+            DataFrame or Series that the xarray object gets converted into
+            before plotting).
+        y : str or list of str, optional
+            Dimension to use for the `y` argument of pandas plotting functions.
+            The dimension(s) listed under this parameter get unstacked onto
+            columns of the DataFrame that the xarray object is converted into,
+            before calling the DataFrame's `plot` function.
+        kind : str, optional
+            What kind of plot to produce. See the documentation of
+            `pandas.DataFrame.plot` for valid options. If not specified, the
+            xarray object will be converted into a DataFrame, and its `plot`
+            function will be called directly.
+        **kwargs
+            Additional keyword arguments to pass to the plotting function. See
+            the documentation of `pandas.DataFrame.plot`.
+        
+        Returns
+        -------
+            Whichever value is returned by the relevant pandas plotting
+            function, typically the return value of a matplotlib plotting
+            function.
+        """
+        pdobj: tp.Union[pd.DataFrame, pd.Series]
+        xrobj: tp.Union[xr.Dataset, xr.DataArray] = self._xrobj
+        if isinstance(xrobj, xr.Dataset):
+            pdobj: pd.DataFrame = xrobj.to_dataframe()
+        elif isinstance(xrobj, xr.DataArray):
+            pdobj: pd.Series = xrobj.to_series()
+        else:
+            pdobj = xrobj.to_pandas()
+        if y is not None:
+            pdobj: pd.DataFrame = pdobj.unstack(y)
+        plotfunc: tp.Callable
+        if kind is None:
+            plotfunc = pdobj.plot
+        else:
+            plotfunc = getattr(pdobj.plot, kind)
+        return plotfunc(**kwargs)
+    ###END def XrPlotUtils.pdplot
+
+    def bar(
+        self,
+        y: str = None,
+        **kwargs
+    ) -> tp.Any:
+        """Make a bar plot leveraging pandas `DataFrame.plot.bar`.
+        
+        See the documentation of the `pdplot` method. This method is equivalent
+        to calling `pdplot` with `kind='bar'`."""
+        return self.pdplot(y=y, kind='bar', **kwargs)
+    ###END dev XrPlotUtils.pdplot
+
+###END class XrPlotUtils
+
 def _get_regfuncs(
     xrtype: tp.Literal['Dataset', 'DataArray', 'both'] = 'both'
 ) -> tp.List[tp.Callable[[str], tp.Callable]]:
@@ -108,3 +193,23 @@ def register_tsutils(
     for _func in regfuncs:
         _func(name)(XrTsUtils)
 ###END def register_tsutils
+
+def register_plotutils(
+    name: str = 'plotutils',
+    xrtype: tp.Literal['Dataset', 'DataArray', 'both'] = 'both'
+):
+    """Reguster XrPlotUtils as xarray accessor `plotutils`.
+    
+    Parameters
+    ----------
+    name : str, optional
+        Name to give to the accessor. Optional, `'plotutils'` by default
+    xrtype : str, optional
+        What xarray object type to register the accessor for. Can be
+        `'Dataset'`, `'DataArray'` or `'both'`. Optional, `'both'` by default.
+    """
+    regfuncs: tp.List[tp.Callable] = _get_regfuncs(xrtype=xrtype)
+    _func: tp.Callable[[str], tp.Callable]
+    for _func in regfuncs:
+        _func(name)(XrPlotUtils)
+###END def register_plotutils
