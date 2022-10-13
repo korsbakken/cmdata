eba9002b68c79027e2141c7ceeb26f1b10215ebc
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2022-10-13 16:36:31 +0200

    Added IEA subcpackage under labels (no content yet)

---
+++ cmdata/labels/iea/__init__.py
@@ -0,0 +1,19 @@
+"""Label definitions and mappings for IEA statistics"""
+import typing as tp
+from pathlib import Path
+
+from ..labelfiles import LabelfileManager
+from ..labelfiles import LabelMap
+
+
+_filemanager: LabelfileManager = LabelfileManager(
+    yamlfiles_root=Path(__file__).parent,
+    yamlfiles={
+        'CO2_bigco2': 'CO2_bigco2_labels.yaml',
+        'WEB_wbal': 'WEB_wbal_labels.yaml'
+    }
+)
+
+yamlfiles: tp.Dict[str, Path] = _filemanager.yamlfiles
+labelsets: tp.Dict[str, tp.List[str]] = _filemanager.labelsets
+get_label_map: tp.Callable[[str, str], LabelMap] = _filemanager.get_label_map
