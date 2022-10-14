ea2e8908cd6a3339e795bd6e7cea2807ead3d445
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2022-10-14 19:10:24 +0200

    Started adding names package, with module for IEA dimension names.

---
+++ cmdata/names/iea.py
@@ -0,0 +1,20 @@
+"""Names to be used with IEA datasets.
+
+So fra only contains a data class of dimension names to be used consisently
+across all IEA data sets.
+"""
+
+import typing as tp
+from dataclasses import dataclass
+
+@dataclass(frozen=True, kw_only=True)
+class DimNames:
+    TIME: str = 'time'
+    REGION: str = 'region'
+    FLOW: str = 'flow'
+    PRODUCT: str = 'product'
+    UNIT: str = 'unit'
+    GAS: str = 'gas'
+###END class dataclass DimNames
+dim_names: DimNames = DimNames()
+    
