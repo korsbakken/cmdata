7313ba1994ba23e803f27ddd4fda074c2808293b
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2023-01-27 08:38:39 +0100

    Added dataload files (just copied from climate_trace_comparison repo).
    Added package files for loading and processing raw data files. These files are
    currently just copied from climate_trace_comparison, and not consistent with
    anything that is done in cmdata.datadesc or anywhere else in the cmdata package.
    Just putting it here to be able to use and modify the code as needed in a
    centrally managed package.

---
+++ cmdata/dataload/__init__.py
@@ -0,0 +1,7 @@
+"""Subpackage for loading data.
+
+At the moment this subpackage is not consistent with the rest of the cmdata
+package. The initial files were copied from the `climate_trace_comparison`, and
+use .ini/.cfg-based config file format read by `ConfigParser`-based classes,
+rather than the yaml-based formats and codes employed by `cmdata.datadesc`.
+"""
