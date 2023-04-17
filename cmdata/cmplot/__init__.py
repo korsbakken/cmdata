8a9cafb13a3b7991ee3cbb13505fd0f559e78bc3
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2023-04-17 13:27:43 +0200

    Added import error handling in import of cmplot in cmdata package.
    Importing the main cmdata package also automatically imports cmdata.cmplot and
    cmdata.cmplot.hv_plotting.  Previously, this would trigger an ImportError if any
    of the plotting package dependencies were not installed. With the new behavior,
    the error is caught and ignored. The submodules/subpackages that could not be
    imported are then not present in the cmdata.cmplots package object, and instead
    the import errors are stored in the dict cmdata.cmplot.submodule_import_errors
    (with the names of the modules/packages as keys).

--- cmdata/cmplot/__init__.py
+++ cmdata/cmplot/__init__.py
@@ -1,2 +1,12 @@
 """Plotting functionality for the CICERO Climate Mitigation group."""
-from . import hv_plotting
+
+# Try to import hv_plotting. If this results in an ImportError, some requisite
+# packages are probably not installed.
+import typing as tp
+
+submodule_import_errors: tp.Dict[str, Exception] = dict()
+
+try:
+    from . import hv_plotting
+except ImportError as ie:
+    submodule_import_errors['hv_plotting'] = ie
